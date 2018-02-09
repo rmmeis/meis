c4474342f3143b3fbb0ab22ffe01802d4173ff1d
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-02-09 15:53:55 +0530

    Create medium.c

---
+++ medium.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,sum=0,i,a[100];
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+printf("median is %d",sum/a);
+}
