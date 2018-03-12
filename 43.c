c097776d25a0e38fa5c2609d6c9b5f31c627b367
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-03-12 15:43:13 +0530

    Update 43.c

--- 43.c
+++ 43.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 include<string.h>
-int main(void)
+int main()
 {
 char a[100],b[100]
 int len1,len2,length,i;
@@ -9,9 +9,10 @@ gets(b);
 len1=strlen(a);
 len2=strlen(b);
 length=len1+len2;
-
 for(i=len1+1;i<length;i++)
-{a[i]=b[j];
+{
+a[i]=b[j];
 }
 printf("%s",a[i]);
 return 0;
+}
