b6344d87b9344f96d01c05d8d70420d093558f00
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-03-12 15:39:19 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+include<string.h>
+int main(void)
+{
+char a[100],b[100]
+int len1,len2,length,i;
+gets(a);
+gets(b);
+len1=strlen(a);
+len2=strlen(b);
+length=len1+len2;
+
+for(i=len1+1;i<length;i++)
+{a[i]=b[j];
+}
+printf("%s",a[i]);
+return 0;
