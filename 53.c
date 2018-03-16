845c8cb2754c25f2823fccd72a3892724b555688
rmmeis <35633122+rmmeis@users.noreply.github.com>
2018-03-16 15:48:04 +0530

    Update 53.c

--- 53.c
+++ 53.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int main(void)
+int main()
 {
-int n,sum=0;
+int a,sum=0;
 scanf("%d",&n);
-while(n!=0)
+while(a!=0)
 {
-sum=sum+(n%10);
-n=n/10;
+sum=sum+(a%10);
+a=a/10;
 }
 printf("%d",sum);
 return 0;
